95645cd1969097acd4771ba5225f3ce116fc9aa4
kholqin <holqinaleavodkabilla@gmail.com>
2026-07-15 23:13:44 +0800

    Create apiConfig.js

---
+++ src/apiConfig.js
@@ -0,0 +1,6 @@
+// src/apiConfig.js
+// Jangan isi key langsung di sini kalau mau di-push ke GitHub!
+// Kita panggil lewat process.env (Vercel akan ngisi ini secara otomatis nanti)
+
+export const API_KEY = import.meta.env.VITE_API_KEY;
+export const BASE_URL = "https://prod-api.dracinapi.com";
